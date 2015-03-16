e8b78f7e340ed33e75dcdafa7957bedcaf0fa314
qiki <qikialg@gmail.com>
2015-03-16 09:11:51 +0800

    code files & PCB arts

---
+++ code/oled/Gekius_common.h
@@ -0,0 +1,24 @@
+/*
+ * Gekius_command.h
+ *
+ *  Created on: 2015Äê3ÔÂ6ÈÕ
+ *      Author: Jhon_Qiki
+ */
+
+#ifndef GEKIUS_COMMAND_H_
+#define GEKIUS_COMMAND_H_
+
+
+enum DevError
+{
+	SUC = 1,
+	FAILED = 2,
+};
+
+enum FunResult
+{
+	FUN_SUC = 0,
+	FUN_FAILED = -1,
+};
+
+#endif /* GEKIUS_COMMAND_H_ */
